99b848af0febaf150a439cf008ae8d79e1656333
cobraaz <anujbansal077@gmail.com>
2021-03-09 00:05:05 +0530

    changes in Home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
-const Home = () => {
-  return <div>Home</div>;
+import { useState } from "react";
+import Head from "next/head";
+
+import { getData } from "../utils/fetchData";
+import ProductItem from "../components/product/ProductItem";
+
+const Home = (props) => {
+  const [products, setProducts] = useState(props.products);
+  console.log(products);
+  return (
+    <div className="products">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      {products.length === 0 ? (
+        <h2>No Products</h2>
+      ) : (
+        products.map((product) => (
+          <ProductItem key={product._id} product={product} />
+        ))
+      )}
+    </div>
+  );
 };
+
+export async function getServerSideProps({ query }) {
+  const res = await getData("product");
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    }, // will be passed to the page component as props
+  };
+}
 
 export default Home;
