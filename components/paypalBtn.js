511aa28b64af31a477093f9ba8147243aa68a4ac
cobraaz <anujbansal077@gmail.com>
2021-03-10 23:58:11 +0530

    Added Profile, Order Page

--- components/paypalBtn.js
+++ components/paypalBtn.js
@@ -1,9 +1,12 @@
-import { useEffect, useRef } from "react";
-import { postData } from "../utils/fetchData";
-const paypalBtn = ({ total, address, mobile, state, dispatch }) => {
-  const refPaypalBtn = useRef();
+import { useEffect, useRef, useContext } from "react";
+import { patchData } from "../utils/fetchData";
+import { DataContext } from "../store/GlobalState";
+import { updateItem } from "../store/Actions";
 
-  const { cart, auth } = state;
+const paypalBtn = ({ order }) => {
+  const refPaypalBtn = useRef();
+  const { state, dispatch } = useContext(DataContext);
+  const { auth, orders } = state;
 
   useEffect(() => {
     paypal
@@ -14,7 +17,7 @@ const paypalBtn = ({ total, address, mobile, state, dispatch }) => {
             purchase_units: [
               {
                 amount: {
-                  value: total,
+                  value: order.total,
                 },
               },
             ],
@@ -22,27 +25,38 @@ const paypalBtn = ({ total, address, mobile, state, dispatch }) => {
         },
         onApprove: function (data, actions) {
           // This function captures the funds from the transaction.
+          dispatch({ type: "NOTIFY", payload: { loading: true } });
+
           return actions.order.capture().then(function (details) {
-            dispatch({
-              type: "NOTIFY",
-              payload: { loading: true },
-            });
-            postData(
-              "order",
-              { address, mobile, cart, total },
+            patchData(
+              `order/${order._id}`,
+              {
+                paymentId: details.payer.payer_id,
+              },
               auth.token
             ).then((res) => {
-              if (res.err) {
+              if (res.err)
                 return dispatch({
                   type: "NOTIFY",
                   payload: { error: res.err },
                 });
-              }
-              dispatch({
-                type: "ADD_CART",
-                payload: [],
-              });
-              dispatch({
+
+              dispatch(
+                updateItem(
+                  orders,
+                  order._id,
+                  {
+                    ...order,
+                    paid: true,
+                    dateOfPayment: details.create_time,
+                    paymentId: details.payer.payer_id,
+                    method: "Paypal",
+                  },
+                  "ADD_ORDERS"
+                )
+              );
+
+              return dispatch({
                 type: "NOTIFY",
                 payload: { success: res.msg },
               });
@@ -53,6 +67,7 @@ const paypalBtn = ({ total, address, mobile, state, dispatch }) => {
       })
       .render(refPaypalBtn.current);
   }, []);
+
   return <div ref={refPaypalBtn}></div>;
 };
 
